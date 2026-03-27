cdef654dcc20a305ade64f597ff5f2fc33a5a31b
M3morui <vivi.santa0320@gmail.com>
2026-03-27 10:41:24 +0100

    model

---
+++ Backend/Model/PaginationParams.cs
@@ -0,0 +1,18 @@
+﻿namespace RegisztracioTest.Model
+{
+    public class PaginationParams
+    {
+        private const int MaxPageSize = 50;
+        private int _pageSize = 10;
+
+        public int Page { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+        public string? SortBy { get; set; } = "newest";
+    }
+}
