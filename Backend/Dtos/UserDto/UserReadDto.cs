f828e41f8ebb417e939c9f78b6ad9c66d707b94f
Bob <csaszarr2004@gmail.com>
2026-03-27 10:34:57 +0100

    Backend

---
+++ Backend/Dtos/UserDto/UserReadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RegisztracioTest.Dtos.UserDto
+{
+    public class UserReadDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? ProfileImages { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? LastActive { get; set; } 
+        public string? Password { get; set; }
+    }
+}
