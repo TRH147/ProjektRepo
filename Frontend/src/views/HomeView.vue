216d011ba81500f5a94c5a02a7b0ec6e471e0830
Bob <csaszarr2004@gmail.com>
2026-03-27 09:11:46 +0100

    Frontend

---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <HeroCover />
+
+
+    <AuthModal />
+
+    <section>
+      <SliderMap />
+    </section>
+
+    <section>
+      <ModesGrid />
+    </section>
+
+    <section>
+      <EquipCards />
+    </section>
+  </main>
+</template>
+
+<script setup>
+import HeroCover from '../components/HeroCover.vue'
+import AuthModal from '../components/AuthModal.vue'
+import SliderMap from '../components/SliderMap.vue'
+import ModesGrid from '../components/ModesGrid.vue'
+import EquipCards from '../components/EquipCards.vue'
+</script>
+
+<style scoped>
+.palya {
+  text-align: center;
+  color: #fae9d7;
+  font-size: 2rem;
+  margin: 60px 0 30px;
+}
+</style>
